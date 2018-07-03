e5f3754ed291f4165b54f32cbf6f610cdeab8c2c
Simpler1 <kevin.bezold@gmail.com>
2018-07-03 19:59:22 -0400

    func(auth): Google login works

--- example/firebase_flutter_repository/lib/user_repository.dart
+++ example/firebase_flutter_repository/lib/user_repository.dart
@@ -6,6 +6,9 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:todos_repository/todos_repository.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 class FirebaseUserRepository implements UserRepository {
   final FirebaseAuth auth;
@@ -14,7 +17,13 @@ class FirebaseUserRepository implements UserRepository {
 
   @override
   Future<UserEntity> login() async {
-    final firebaseUser = await auth.signInAnonymously();
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final firebaseUser = await auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
 
     return UserEntity(
       id: firebaseUser.uid,
